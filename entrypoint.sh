bee09d9e1525ad9c8c32fd98d2313befd505421d
sslavv <64302974+sslavv@users.noreply.github.com>
2023-09-25 08:55:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ cat config.json | base64 > config
 rm -f config.json
 
 # 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
-TLS=${NEZHA_TLS:+'--tls'}
+#TLS=${NEZHA_TLS:+'--tls'}
 #[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY} ${TLS}
 # 下载最新版本 Nezha Agent
 download_agent() {
